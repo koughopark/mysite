15ee012b258534affd37dc38657d021a1ecf84a4
KevinParkKorea <kevin929292@naver.com>
2018-06-30 13:55:46 +0900

    board html연결

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,4 @@
 """mysite URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -15,11 +14,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import main.views as main_views
 
+import main.views as main_views
+import user.views as user_views
+import guestbook.views as guestbook_views
+import board.views as board_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', main_views.index),
+
+    path('user/joinform/', user_views.joinform),
+    path('user/join', user_views.join),
+    path('user/joinsuccess/', user_views.joinsuccess),
+    path('user/loginform/', user_views.loginform),
+    path('user/login', user_views.login),
+    path('user/logout', user_views.logout),
+
+    path('guestbook/', guestbook_views.list),
+    path('guestbook/add', guestbook_views.add),
+    path('guestbook/deleteform/', guestbook_views.deleteform),
+    path('guestbook/delete', guestbook_views.delete),
+
+    path('board/', board_views.list),
+    path('board/view', board_views.view),
+    path('board/wirte', board_views.write),
+    path('board/modify', board_views.modify),
+
+    path('admin/', admin.site.urls),
 
 ]
